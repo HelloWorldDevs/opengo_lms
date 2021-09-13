1e9648957c5aa1011ed8991439f5a6e13459c612
geophysicist <vsiekan@connect-i.ch>
2021-09-13 18:26:37 +0300

    removed multiselect

--- modules/opigno_user_selection/widget/userselect.js
+++ modules/opigno_user_selection/widget/userselect.js
@@ -1,32 +1,39 @@
 'use strict';
 
 (function ($) {
-  Drupal.behaviors.opignoCatalog = {
+
+  $.fn.generateProps = function () {
+    var props = {};
+    $(this).each(function () {
+      $.each(this.attributes, function () {
+        if (this.specified) {
+          props['data-' + this.name] = this.value;
+        }
+      });
+      var user_data_list = {}
+      $('option', this).each(function (i, e) {
+        user_data_list[jQuery(e).val()] = $(e).text()
+      })
+      props['data-user-list'] = user_data_list;
+      props['data-user-default'] = jQuery(this).val();
+    });
+    return props;
+  };
+
+  Drupal.behaviors.opignoUserSelection = {
     attach: function (context, settings) {
-      $.fn.generateProps = function () {
-        var props = {};
-        $(this).each(function () {
-          $.each(this.attributes, function () {
-            if (this.specified) {
-              props['data-' + this.name] = this.value;
-            }
-          });
-          var user_data_list = {}
-          $('option', this).each(function (i, e) {
-            user_data_list[jQuery(e).val()] = $(e).text()
-          })
-          props['data-user-list'] = user_data_list;
-          props['data-user-default'] = jQuery(this).val();
-        });
-        return props;
-      };
+      var userselect = window.userselect || false;
+      if(!userselect) {
+        return;
+      }
+
       $('[data-user-load]', context).each(function (index, element) {
-        var props = $(element).generateProps();
+        var properties = $(element).generateProps();
         new Vue({
           el: element,
           render: function (h) {
             return h(userselect, {
-              props,
+              props: properties,
             })
           }
         })
